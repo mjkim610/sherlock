d42c0b11969eb79e848601c720bcf197bb827c3c
mjkim610 <mjkim610@gmail.com>
2016-10-12 20:25:14 +0900

    add sample signup page

--- lib/signup.php
+++ lib/signup.php
@@ -1,1 +1,38 @@
-a
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Sherlock - Sign Up</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/signup.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <form class="form-signup">
+            <h2 class="form-signup-heading">Please Sign Up</h2>
+            <label for="inputUsername" class="sr-only">Username</label>
+            <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+            <br />
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <label for="checkPassword" class="sr-only">Retype Password</label>
+            <input type="password" id="checkPassword" class="form-control" placeholder="Retype Password" required>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" value="save-fp"> Save current browser fingerprint
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
+        </form>
+
+    </div>
+</body>
+
+</html>
