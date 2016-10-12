28f8cfc11cdb97817c197ce659570d42131cd4fd
mjkim610 <mjkim610@gmail.com>
2016-10-12 20:30:14 +0900

    change login.php style

--- lib/login.php
+++ lib/login.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html>
-<html lang="en-us">
 
 <head>
-	<meta charset="utf-8">
-	<title>Login Page</title>
+    <meta charset="utf-8">
 
-	<link rel="stylesheet" type="text/css" href="/css/login.css"></link>
+    <title>Sherlock - Sign Up</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/signup.css" rel="stylesheet">
 </head>
 
 <body>
-	<div class="center">
-	    <form class="center">
-	        Username: <input type="text" id="username"></input> <br/>
-	        <input type="submit" id="login" value="Login"></input>
-	    </form>
-	</div>
 
-    <script src="/js/login.js"></script>
+    <div class="container">
+        <form class="form-signup">
+            <h2 class="form-signup-heading">Please Log In</h2>
+            <label for="inputUsername" class="sr-only">Username</label>
+            <input type="text" id="username" class="form-control" placeholder="Username" required autofocus>
+            <br />
+            <label for="inputPin" class="sr-only">PIN</label>
+            <input type="password" id="inputPin" class="form-control" placeholder="PIN" required>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" value="save-fp"> Save current browser fingerprint
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
+        </form>
+
+    </div>
+
+	<script src="/js/login.js"></script>
 </body>
 
 </html>
