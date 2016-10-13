a64dfaa5b146ef885d8e0d62ac8fd8ab94b3bffd
mjkim610 <mjkim610@gmail.com>
2016-10-13 18:01:38 +0900

    move font-awesome directory and refactor footer.php

--- lib/footer.php
+++ lib/footer.php
@@ -1,71 +1,68 @@
-  <footer class="container-fluid text-center">
+<footer class="container-fluid text-center">
     <a href="#" title="To Top">
-      <span class="glyphicon glyphicon-chevron-up"></span>
+        <span class="glyphicon glyphicon-chevron-up"></span>
     </a>
 
-  <!-- Team Section -->
-  <section id="team">
-      <div class="container-fluid text-center">
-          <div class="row">
-              <div class="col-lg-12 text-center">
-                  <h2 class="section-heading">Our Amazing Team</h2>
-                  <h3 class="section-subheading text-muted">협업실 B01</h3>
-              </div>
-          </div>
-          <div class="row">
-              <div class="col-sm-4">
-                  <div class="team-member">
-                      <img src="img/team/1.jpg" class="img-profile img-responsive img-circle" alt="image">
-                      <h4>정술람</h4>
-                      <p class="text-muted">Lead Developer</p>
-                      <ul class="list-inline social-buttons">
-                          <li><a href="#"><i class="fa fa-twitter"></i></a>
-                          </li>
-                          <li><a href="#"><i class="fa fa-facebook"></i></a>
-                          </li>
-                          <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-              <div class="col-sm-4">
-                  <div class="team-member">
-                      <img src="img/team/2.jpg" class="img-profile img-responsive img-circle" alt="image">
-                      <h4>김정헌</h4>
-                      <p class="text-muted">Lead Developer</p>
-                      <ul class="list-inline social-buttons">
-                          <li><a href="#"><i class="fa fa-twitter"></i></a>
-                          </li>
-                          <li><a href="#"><i class="fa fa-facebook"></i></a>
-                          </li>
-                          <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-              <div class="col-sm-4">
-                  <div class="team-member">
-                      <img src="img/team/3.jpg" class="img-profile img-responsive img-circle" alt="image">
-                      <h4>김명종</h4>
-                      <p class="text-muted">Lead Developer</p>
-                      <ul class="list-inline social-buttons">
-                          <li><a href="#"><i class="fa fa-twitter"></i></a>
-                          </li>
-                          <li><a href="#"><i class="fa fa-facebook"></i></a>
-                          </li>
-                          <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-          <div class="row">
-              <div class="col-lg-8 col-lg-offset-2 text-center">
-                  <p class="large text-muted">연세대학교 소프트웨어종합설계 팀 협업실 B01입니다.</p>
-              </div>
-          </div>
-      </div>
+    <!-- Team Section -->
+    <section id="team">
+        <div class="container-fluid text-center">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Our Amazing Team</h2>
+                    <h3 class="section-subheading text-muted">협업실 B01</h3>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/1.jpg" class="img-profile img-responsive img-circle" alt="image">
+                        <h4>정술람</h4>
+                        <p class="text-muted">Lead Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="https://github.com/sullamij"><i class="fa fa-github"></i></a> </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a> </li>
+                            <li><a href="mailto:sullamij@naver.com?Subject=Regarding%20Sherlock"><i class="fa fa-envelope"></i></a> </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/2.jpg" class="img-profile img-responsive img-circle" alt="image">
+                        <h4>김정헌</h4>
+                        <p class="text-muted">Lead Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="https://github.com/restforest"><i class="fa fa-github"></i></a> </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a> </li>
+                            <li><a href="mailto:jhoney7374@gmail.com?Subject=Regarding%20Sherlock"><i class="fa fa-envelope"></i></a> </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/3.jpg" class="img-profile img-responsive img-circle" alt="image">
+                        <h4>김명종</h4>
+                        <p class="text-muted">Lead Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="https://github.com/mjkim610"><i class="fa fa-github"></i></a> </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a> </li>
+                            <li><a href="mailto:mjkim610@gmail.com?Subject=Regarding%20Sherlock"><i class="fa fa-envelope"></i></a> </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <p class="large text-muted">연세대학교 소프트웨어종합설계 팀 협업실 B01입니다.</p>
+                </div>
+            </div>
+
+        </div>
     </section>
-  </footer>
+</footer>
 </body>
+
 </html>
--- lib/head.php
+++ lib/head.php
@@ -13,7 +13,7 @@
     <!-- <meta property="og:url" content="http://.kr/"> -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome-4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="css/style.css" rel="stylesheet" media="screen">
     <link href="css/agency.css" rel="stylesheet" media="screen">
     <link href="css/login-signup.css" rel="stylesheet" media="screen">
