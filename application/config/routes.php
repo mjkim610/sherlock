b79c6d13807703ff33f6502c540f80e19c499ba8
restforest <jhoney7374@gmail.com>
2017-01-23 20:46:30 +0900

    edit route file

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,7 +5,12 @@ $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['about'] = 'home/about';
 $route['login'] = 'welcome/login';
 $route['signup'] = 'welcome/signup';
 $route['provider-signup'] = 'welcome/provider_signup';
+
+$route['about'] = 'home/about';
+$route['user'] = 'home/edit';
+
+$route['my/fingerprint'] = 'user/fingerprint';
+$route['my/app'] = 'provider/my_app';
