5a41972ea856cb9e0b8db99f4803ebac7a0c177c
restforest <jhoney7374@gmail.com>
2017-02-02 19:23:10 +0900

    change javascript code to jquery

--- application/views/home/main_body.php
+++ application/views/home/main_body.php
@@ -14,5 +14,15 @@
 <div class="row">
   <div class="col-sm-10 col-sm-offset-1">
     메인 페이지
+    <button class="btn btn-primary" type="button" id="sherlock-btn" name="button">Try-it</button>
   </div>
 </div>
+
+<script type="text/javascript">
+$('#sherlock-btn').click(function() {
+  var get_string = '?';
+  get_string = get_string + 'type=' + encodeURI('fingerprint pin password');
+  get_string = get_string + '&token=' + encodeURI('Token Place');
+  location.href = 'http://localhost/sherlock/sherlock/auth' + get_string;
+});
+</script>
