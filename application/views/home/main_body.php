f279f96e357c63fa38e4ab4096761e9244b6eae0
restforest <jhoney7374@gmail.com>
2017-02-09 15:41:30 +0900

    add fingerprint test box

--- application/views/home/main_body.php
+++ application/views/home/main_body.php
@@ -12,9 +12,19 @@
   }
 ?>
 <div class="row">
-  <div class="col-sm-10 col-sm-offset-1">
-    메인 페이지
-    <button class="btn btn-primary" type="button" id="sherlock-btn" name="button">Try-it</button>
+  <div class="col-sm-12">
+    <div class="col-sm-10 col-sm-offset-1">
+      메인 페이지
+      <button class="btn btn-primary" type="button" id="sherlock-btn" name="button">Try-it</button>
+    </div>
+  </div>
+  <div class="col-sm-12">
+    <div class="col-sm-10 col-sm-offset-1">
+      <button class="btn btn-primary" type="button" id="sherlock-test-btn" name="button">Test your fingerprint</button>
+      <div>
+        <span id="components"></span>
+      </div>
+    </div>
   </div>
 </div>
 
@@ -46,6 +56,28 @@ $('#sherlock-btn').click(function() {
       // get_string = get_string + '&redirect_uri=' + encodeURI('Token Place'); // ajax_result['redirect_uri']
       location.href = 'http://localhost/sherlock/authentication' + get_string;
     }
+  });
+});
+</script>
+<script type="text/javascript">
+$('#sherlock-test-btn').click(function() {
+  var d1 = new Date();
+  var fp = new Fingerprint2();
+  fp.get(function(result, components) {
+      var d2 = new Date();
+      var timeString = "Time took to calculate the fingerprint: " + (d2 - d1) + "ms";
+
+      var output = '';
+      var linenumber = 1;
+      for (var property in components) {
+          output += linenumber + ': <b>' + components[property]['key'] + '</b><br>' + String(components[property]['value']).substring(0, 1248)+'<br><br>';
+          linenumber++;
+      }
+
+
+      $("#fp").text(result);
+      $("#time").text(timeString);
+      $("#components").html(output);
   });
 });
 </script>
